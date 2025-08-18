2ece2201e759b523d11ca683c2cfb3d6f96ef75e
cschere1 <cschere1@gmail.com>
2025-08-17 21:04:10 -0500

    Updating landing page

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -5,10 +5,6 @@ export default function LandingPage() {
   return (
     <main className={styles.main}>
       <Hero />
-      <section id="projects" className={styles.section}>
-        <h2 style={{ marginTop: 0 }}>Projects</h2>
-        <p className="subtle">Coming soon…</p>
-      </section>
     </main>
   );
 }
